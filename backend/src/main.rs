536ca7a4035ccbbadfe49621e75db0b837291c4d
jm <j.maxwell.alan@gmail.com>
2022-02-06 12:16:15 -0500

    create basic test route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-  println!("Hello, world!");
+use actix_web::{HttpServer, App};
+mod routes;
+use routes::hello;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  HttpServer::new(|| {
+    App::new()
+    .service(hello)
+  })
+  .bind("localhost:5000")
+  .unwrap()
+  .run()
+  .await
 }
