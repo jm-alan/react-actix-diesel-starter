70aa91194e2eaaabc84ef7121e58036924b79343
jm <j.maxwell.alan@gmail.com>
2022-02-06 12:40:29 -0500

    useSelector -> useTypedSelector

--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { AppState } from '../../store';
+import { useTypedSelector } from '../../hooks/redux';
 
-export default function Modal(): JSX.Element {
-  const display = useSelector((state: AppState) => state.modal.display);
-  const Current = useSelector((state: AppState) => state.modal.Current);
+export default function Modal (): JSX.Element | null {
+  const display = useTypedSelector(state => state.modal.display);
+  const Current = useTypedSelector(state => state.modal.Current);
 
-  return display && Current && (
-    <div className='fixed h-0 w-0'>
-      <div className='fixed inset-0 h-screen w-screen bg-black opacity-70 flex items-center justify-center'>
-        <div className='h-2/3 bg-gray-700 w-2/3 rounded-2xl flex items-center justify-center'>
-          <Current />
+  return (
+    display && Current && (
+      <div className='fixed h-0 w-0'>
+        <div className='fixed inset-0 h-screen w-screen bg-black opacity-70 flex items-center justify-center'>
+          <div className='h-2/3 bg-gray-700 w-2/3 rounded-2xl flex items-center justify-center'>
+            <Current />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  ) || null;
 }
